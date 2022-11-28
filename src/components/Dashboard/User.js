65b28650594c19a43fac537abd9cb575873f7e17
Md Mamun <juborajvai22@gmail.com>
2022-11-28 15:47:27 +0600

    update

--- src/components/Dashboard/User.js
+++ src/components/Dashboard/User.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 import { AiFillDelete } from "react-icons/ai";
+import swal from "sweetalert";
 import Loading from "../Utilites/Loading";
 
 const User = () => {
@@ -7,7 +9,28 @@ const User = () => {
   const [owner, setOwner] = useState([]);
   const [adviser, setAdviser] = useState([]);
   const [loading, setLoading] = useState(false);
-
+  const [show, setShow] = useState(false);
+  const [myUser, setMyUser] = useState({});
+  const [id, setId] = useState("");
+  const [role, setRole] = useState("");
+  const handleClose = () => setShow(false);
+  const [fetchData, setFetch] = useState(false);
+  const [userRole, setUserRole] = useState("user");
+  const handleShow = (id) => {
+    setId(id);
+    setShow(true);
+    fetch(`https://ancient-earth-39666.herokuapp.com/api/v1/user/single/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          setMyUser(data?.user);
+          // setUser(data?.user);
+          // setLoading(false);
+        } else {
+          // setLoading(false);
+        }
+      });
+  };
   useEffect(() => {
     setLoading(true);
     //     const userId = localStorage.getItem("userId");
@@ -17,6 +40,7 @@ const User = () => {
         if (data.success) {
           setUser(data?.user);
           setLoading(false);
+          setFetch(false);
         } else {
           setLoading(false);
         }
@@ -41,11 +65,76 @@ const User = () => {
           setLoading(false);
         }
       });
-  }, []);
+  }, [fetchData]);
 
   if (loading) {
     return <Loading />;
   }
+
+  const makeUserAdminHendeler = () => {
+    fetch(
+      `http://localhost:5000/api/v1/user/admin/${myUser?.email}?roleAction=${role}`,
+      {
+        method: "PUT",
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("Token")}`,
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          // disPatch(fetchUser());
+          // disPatch(fetchAdmin());
+          // toast("Admin Make Successfull");
+          // closeModal();
+          setFetch(true);
+          handleClose();
+          swal({
+            title: "Admin Make Succssfull",
+            text: "Thank you Sir",
+            icon: "success",
+            buttons: [false],
+          });
+        }
+      });
+  };
+
+  const action = [
+    { name: "Please Select Role" },
+    { name: "advaiser" },
+    { name: "owner" },
+  ];
+
+  const removeAdminHendeler = (userss) => {
+    console.log(userss)
+    fetch(
+      `http://localhost:5000/api/v1/user/remove/${userss}?roleAction=${userRole}`,
+      {
+        method: "PUT",
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("Token")}`,
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          // disPatch(fetchUser());
+          // disPatch(fetchAdmin());
+          // toast("Admin Make Successfull");
+          // closeModal();
+          setFetch(true);
+          handleClose();
+          swal({
+            title: "Remove Admin Succssfull",
+            text: "Thank you Sir",
+            icon: "success",
+            buttons: [false],
+          });
+        }
+      });
+  };
 
   return (
     <div className="my-5">
@@ -70,7 +159,103 @@ const User = () => {
                     <td>{user.email}</td>
                     <td>{user.role}</td>
                     <td>
-                      <button className="btn btn-warning">Make Admin</button>
+                      <Modal show={show} onHide={handleClose}>
+                        <div className=" p-5">
+                          <Modal.Title className="text-center">
+                            <div className="">
+                              <span className="label">Name</span>
+                              <input
+                                // {...register("name", {
+                                //   required: {
+                                //     value: true,
+                                //     message: "Name is Required",
+                                //   },
+                                // })}
+                                value={myUser?.name}
+                                placeholder="Course Name"
+                                className="shippingInput"
+                                type="text"
+                                name="name"
+                                id="name"
+                              />
+                            </div>
+                            <div className="mt-2">
+                              <span className="label">Email</span>
+                              <input
+                                // {...register("name", {
+                                //   required: {
+                                //     value: true,
+                                //     message: "Name is Required",
+                                //   },
+                                // })}
+                                value={myUser?.email}
+                                placeholder="Course Name"
+                                className="shippingInput"
+                                type="text"
+                                name="name"
+                                id="name"
+                              />
+                            </div>
+                            <div className="mt-2">
+                              <span className="label">Role</span>
+                              {/* <label for="role" class="block text-sm text-black ">
+                          Role
+                        </label> */}
+                              <select
+                                // {...register("name", {
+                                //   required: {
+                                //     value: true,
+                                //     message: "Name is Required",
+                                //   },
+                                // })}
+                                onChange={(e) => setRole(e.target.value)}
+                                value={myUser?.email}
+                                // placeholder="Course Name"
+                                className="shippingInput"
+                                type="select"
+                                name="role"
+                                id="role"
+                              >
+                                {/* <option value="Please Select Role">
+                                  Please Select Role
+                                </option> */}
+                                {/* <option value="advaiser" selected>
+                                  Advaiser
+                                </option>
+                                <option value="owner">Owner</option> */}
+                                {action.map((role) => (
+                                  <option key={role.name} value={role.name}>
+                                    {role.name}
+                                  </option>
+                                ))}
+                              </select>
+                            </div>
+                          </Modal.Title>
+
+                          <div className=" d-flex justify-content-center mt-2 px-4">
+                            <Button
+                              // variant="secondary"
+                              className="btn btn-warning w-100"
+                              onClick={() => makeUserAdminHendeler()}
+                            >
+                              Make Admin
+                            </Button>
+                          </div>
+                        </div>
+                        {/* <Modal.Header closeButton> */}
+
+                        {/* <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body> */}
+
+                        {/* <Modal.Footer>
+         
+        </Modal.Footer> */}
+                      </Modal>
+                      <button
+                        onClick={() => handleShow(user?._id)}
+                        className="btn btn-warning"
+                      >
+                        Make Admin
+                      </button>
                     </td>
 
                     <td>
@@ -93,8 +278,8 @@ const User = () => {
                   <th scope="col">Name</th>
                   <th scope="col">Email</th>
                   <th scope="col">Role</th>
-                  <th scope="col">Make Admin</th>
-                  <th scope="col">Action</th>
+                  {/* <th scope="col">Make Admin</th> */}
+                  <th scope="col">Remove Owner</th>
                 </tr>
               </thead>
               <tbody>
@@ -103,12 +288,12 @@ const User = () => {
                     <th scope="row">{user?.name}</th>
                     <td>{user.email}</td>
                     <td>{user.role}</td>
-                    <td>
+                    {/* <td>
                       <button className="btn btn-warning">Make Admin</button>
-                    </td>
+                    </td> */}
 
                     <td>
-                      <span className="delete-btn">
+                      <span onClick={()=>removeAdminHendeler(user?.email)} className="delete-btn">
                         <AiFillDelete />
                       </span>
                     </td>
@@ -127,8 +312,8 @@ const User = () => {
                   <th scope="col">Name</th>
                   <th scope="col">Email</th>
                   <th scope="col">Role</th>
-                  <th scope="col">Make Admin</th>
-                  <th scope="col">Action</th>
+                  {/* <th scope="col">Make Admin</th> */}
+                  <th scope="col">Remove Woner</th>
                 </tr>
               </thead>
               <tbody>
@@ -137,12 +322,12 @@ const User = () => {
                     <th scope="row">{user?.name}</th>
                     <td>{user.email}</td>
                     <td>{user.role}</td>
-                    <td>
+                    {/* <td>
                       <button className="btn btn-warning">Make Admin</button>
-                    </td>
+                    </td> */}
 
                     <td>
-                      <span className="delete-btn">
+                      <span onClick={()=>removeAdminHendeler(user?.email)} className="delete-btn">
                         <AiFillDelete />
                       </span>
                     </td>
